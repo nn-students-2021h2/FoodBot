91aa44b602d92acd531f8ba69dec89db144a18d2
Alex_Ershov <AlexWE36@mail.ru>
2021-11-29 21:33:35 +0300

    adding task_1 and update task_3

--- Ershov_Alexey/Home_Work_2/task_3/unixtime_packages/setup.py
+++ Ershov_Alexey/Home_Work_2/task_3/unixtime_packages/setup.py
@@ -1,17 +1,20 @@
 from setuptools import setup
 
 setup(
-    name='pretty_time_package',
+    name='unixtime_package',
     version='0.1',
-    description='allows you to get unixtime in pretty format',
+    description='allows you to get unixtime',
     url='http://github.com/name/package_name',
     author='Alex',
     author_email='AlexVE36@yandex.ru',
     license='MIT',
-    packages=['get_pretty_time_package'],
+    packages=['get_unixtime_package'],
+    install_requires=[
+        'requests==2.26.0',
+    ],
     entry_points={
         'console_scripts': [
-            'get_time=get_pretty_time_package.pretty_time:main'
+            'get_time=get_unixtime_package.unixtime:main',
         ]
     }
 )
