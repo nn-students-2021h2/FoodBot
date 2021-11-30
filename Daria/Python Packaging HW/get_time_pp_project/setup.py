2367bb9a856fbfe86498c498b2db6d869ac63b35
Daria Malikova <55386345+damalikova@users.noreply.github.com>
2021-11-30 13:19:24 +0300

    upload correct setup.py file

--- Daria/Python Packaging HW/get_time_pp_project/setup.py
+++ Daria/Python Packaging HW/get_time_pp_project/setup.py
@@ -1,19 +1,21 @@
 from setuptools import setup
 
 setup(
-    name='get_time_package',
+    name='get_time_pp_package',
     version='0.1',
     description='description',
     url='http://github.com/name/package_name',
     author='Daria Malikova',
     author_email='email@example.com',
     license='MIT',
-    packages=['get_time_package'],
+    packages=['get_time_pp_package'],
     install_requires=[
-        'requests==2.26.0'],
+        'requests==2.26.0',
+        'get_time_package==0.1'
+    ],
     entry_points={
         'console_scripts': [
-            'get_time=get_time_package.get_time_module:main'
+            'get_time=get_time_pp_package.get_time_pp_module:main'
         ]
     }
 )
