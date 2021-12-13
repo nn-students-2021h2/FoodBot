be68fb960737f1f1859fded617f8dbca7bc66b39
Alex_Ershov <AlexWE36@mail.ru>
2021-12-13 16:42:16 +0300

    bot's skeleton

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+# -*- coding: utf-8 -*-
+TOKEN = '5038288042:AAHIZfCj2HqCmUlTrVMt5oQU5TmHAL9Fcco'  # Insert your bot token here - required!
+# Optionally use an anonymizing proxy (SOCKS/HTTPS) if you encounter Telegram access issues in your region
+PROXY = ''
+
+setup(
+    name='food_bot',
+    version='0.1',
+    description='',
+    url='',
+    author='',
+    author_email='',
+    license='',
+    packages=[''],
+    install_requires=[
+        '',
+    ],
+    entry_points={
+        'console_scripts': [
+            '',
+        ]
+    }
+)
