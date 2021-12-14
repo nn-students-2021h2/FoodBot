54dfbb60de17f2d78f021fb249e424d10fb0adef
Daria Malikova <55386345+damalikova@users.noreply.github.com>
2021-12-14 19:19:51 +0300

    upload `meal_class` skeleton

---
+++ meal_class.py
@@ -0,0 +1,24 @@
+class Meal:
+
+    def __init__(self, dish, portion_size, measure_units, date=None, time=None):
+        self.dish = dish
+        self.portion_size = portion_size
+        self.measure_units = measure_units
+        self.date = date
+        self.time = time
+        self._calories = 0
+        self._proteins = 0
+        self._fats = 0
+        self._carbohydrates = 0
+
+    def count_nutrients(self):
+        """
+        counts nutrition facts based on dish data stored in dish_database
+        """
+        pass
+
+    def meal_to_database(self):
+        """
+        Adds each dish to a database
+        """
+        pass
