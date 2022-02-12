852a265fac0a628a1c4227263bae70ab2bee6425
Alex Ershov <AlexWE36@mail.ru>
2022-02-12 16:22:35 +0300

    meal_class parametrs update

--- sourse/meal_class.py
+++ sourse/meal_class.py
@@ -1,21 +1,27 @@
 class Meal:
     def __init__(
         self,
+        user_id: int,
+        meal_id: int,
         dish: str,
-        portion_size: int,
-        measure_units: str,
+        meal_size: int,
+        average_calories: int = 0,
+        average_proteins: int = 0,
+        average_fats: int = 0,
+        average_carbohydrates: int = 0,
         date: str = None,
         time: str = None,
     ):
+        self.user_id = user_id
+        self.meal_id = meal_id
         self.dish = dish
-        self.portion_size = portion_size
-        self.measure_units = measure_units
+        self.meal_size = meal_size
+        self._calories = (average_calories/100)*meal_size
+        self._proteins = (average_proteins/100)*meal_size
+        self._fats = (average_fats/100)*meal_size
+        self._carbohydrates = (average_carbohydrates/100)*meal_size
         self.date = date
         self.time = time
-        self._calories = 0
-        self._proteins = 0
-        self._fats = 0
-        self._carbohydrates = 0
 
     def count_nutrients(self):
         """
